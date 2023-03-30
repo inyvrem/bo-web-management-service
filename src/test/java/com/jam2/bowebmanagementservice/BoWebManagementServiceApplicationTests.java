92602a064834f13e2af96ed2ad8963a8248391e8
Mervyn <mervynjie@gmail.com>
2023-03-30 22:10:18 +0800

    base

---
+++ src/test/java/com/jam2/bowebmanagementservice/BoWebManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jam2.bowebmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoWebManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
